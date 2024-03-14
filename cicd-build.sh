f41bc14b1b603cf5a06074e18ec144d91402c6d7
Vitor Silva <vitor.azevedo009@gmail.com>
2024-03-14 07:59:11 -0300

    Updated cicd script

--- cicd-build.sh
+++ cicd-build.sh
@@ -4,11 +4,8 @@ ROOT=$(pwd)
 
 cd "$APP"
 
-./gradlew clean
-./gradlew build
-./gradlew build --write-versions
-APP_VERSION=$(./gradlew properties -q | grep "version:" | awk '{print $2}')
-./gradlew build --write-versions
+APP_VERSION=$(./gradlew properties -q | grep "version:" | awk '{ sub("-SNAPSHOT", "", $2); print $2 }')
+./gradlew clean build
 
 git add build.gradle
 git commit -m "cicd: bump version ${APP}:${APP_VERSION}"
